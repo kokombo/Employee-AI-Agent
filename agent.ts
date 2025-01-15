803ad79cecc08ccf7c6ca4ac228333dcc0bc1040
kokombo <samuelibrahim3029@gmail.com>
2025-01-15 12:36:02 +0100

    AI agent

--- agent.ts
+++ agent.ts
@@ -1,11 +1,15 @@
 import { OpenAIEmbeddings } from "@langchain/openai";
 import { ChatAnthropic } from "@langchain/anthropic";
-import { AIMessage, BaseMessage, HumanMessage } from "@langchain/core/messages";
+import {
+  AIMessage,
+  type BaseMessage,
+  HumanMessage,
+} from "@langchain/core/messages";
 import {
   ChatPromptTemplate,
   MessagesPlaceholder,
 } from "@langchain/core/prompts";
-import { StateGraph, Annotation } from "@langchain/langgraph";
+import { type StateGraph, Annotation } from "@langchain/langgraph";
 import { tool } from "@langchain/core/tools";
 import { ToolNode } from "@langchain/langgraph/prebuilt";
 import { MongoDBSaver } from "@langchain/langgraph-checkpoint-mongodb";
@@ -22,4 +26,68 @@ export const callAgent = async (
   const dbName = "hr_database";
   const db = client.db(dbName);
   const collection = db.collection("employees");
+
+  const GraphState = Annotation.Root({
+    messages: Annotation<BaseMessage[]>({
+      reducer: (x, y) => x.concat(y),
+    }),
+  });
+
+  const employeeLookupTool = tool(
+    async ({ query, n = 10 }) => {
+      const dbConfig = {
+        collection: collection,
+        indexName: "vector_index",
+        textKey: "embedding_text",
+        embeddingKey: "embedding",
+      };
+
+      const vectorStore = new MongoDBAtlasVectorSearch(
+        new OpenAIEmbeddings(),
+        dbConfig
+      );
+
+      const result = await vectorStore.similaritySearch(query, n);
+      return JSON.stringify(result);
+    },
+    {
+      name: "employee_lookup",
+      description: "Gather employee information from the HR database",
+      schema: z.object({
+        query: z.string().describe("The search query"),
+        n: z
+          .number()
+          .optional()
+          .default(10)
+          .describe("Number of results to return"),
+      }),
+    }
+  );
+
+  const tools = [employeeLookupTool];
+
+  //GraphState.State
+  const toolNode = new ToolNode<typeof GraphState.State>(tools);
+
+  const model = new ChatAnthropic({
+    model: "claude-3-5-sonnet-20240620",
+    temperature: 0,
+  }).bindTools(tools);
+
+  const callModel = async (state: typeof GraphState.State) => {
+    const prompt = ChatPromptTemplate.fromMessages([
+      [
+        "system",
+        `You are a helpful AI assistant, collaborating with other assistants. Use the provided tools to progress towards answering the question. If you are unable to full answer, that's ok, another assistant with different tools will help where you left off. Execute what you can to make progress. If you or any of the other assistants have the final answer of deliverable, prefix your response with FINAL ANSWER so that the team knows to stop. You have access to the following tools: (tool_names).\n(system_message)\nCurrent time: (time).`,
+      ],
+      new MessagesPlaceholder("messages"),
+    ]);
+
+    const formattedPrompt = await prompt.formatMessages({
+      system_message: " You are a helpul HR Chatbot Agent",
+      time: new Date().toISOString(),
+      tool_names: tools.map((tool) => tool.name).join(", "),
+      messages: state.messages,
+    });
+  };
 };
